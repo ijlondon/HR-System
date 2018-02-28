5a7e50e7a6d23a637163734b352f9be7185184f1
secretspy97 <nathan.connor@gosparx.org>
2018-02-28 08:38:44 -0500

    added models and user controller

--- backend/HR-System/src/main/java/com/rit/group2/controllers/AccountController.java
+++ backend/HR-System/src/main/java/com/rit/group2/controllers/AccountController.java
@@ -1,13 +1,35 @@
 package com.rit.group2.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.rit.group2.models.Employee;
+
+@RestController("/user")
+@EnableAutoConfiguration
 public class AccountController {
 
-	@RequestMapping("/greeting")
-    public String greeting() {
-        return "greeting";
+	@RequestMapping(method = RequestMethod.POST, value = "/create")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employee;
     }
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/{userId}")
+    public Employee getEmployee(@PathVariable("userId") int employeeId) {
+        return new Employee("Nathan", "Connor", "home", "(585) 111-1111", "nxc5929@rit.edu");
+    }
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/{userId}/edit")
+    public Employee editEmployee(
+    		@PathVariable("userId") int employeeId,
+    		@RequestBody Employee employee) {
+        return new Employee("Nathan", "Connor", "home", "(585) 222-1126", "nxc5929@rit.edu");
+    }
+	
+	
 }
