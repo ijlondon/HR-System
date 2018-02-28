8afe180ea7ce9f8470eef204885f2c41042ac7bc
Alex Michael Christodoulou Jr <amc8391@rit.edu>
2018-02-28 17:45:15 -0500

    Starting API interaction between front & backend

--- backend/HR-System/src/main/java/com/rit/group2/controllers/EmployeeController.java
+++ backend/HR-System/src/main/java/com/rit/group2/controllers/EmployeeController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.rit.group2.models.Employee;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")
 @EnableAutoConfiguration
 public class EmployeeController {
 
