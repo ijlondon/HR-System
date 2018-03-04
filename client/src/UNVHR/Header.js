af49905a3c9434566d78b233c9ac14dab748de08
HuseenMK <hjm3628@rit.edu>
2018-03-04 18:20:37 -0500

    Home Page is done .. Backend connection is needed

--- client/src/UNVHR/Header.js
+++ client/src/UNVHR/Header.js
@@ -15,7 +15,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {Link} from 'react-router';
 import './Header.css';
 import logo from './logo.png';
-  
+import {SearchBox} from './SearchBox'
 
 
   export class Header extends React.Component {
@@ -36,10 +36,16 @@ import logo from './logo.png';
       return (
         <div>
           <Navbar color="faded" light expand="md">
-            <NavbarBrand href="/">
-            <img src={logo} style={{width:60, marginTop: -7, padding:'5px'}} />
-            University HR</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
+            
+            <NavbarBrand href="/Home">
+                <img src={logo} style={{width:60, marginTop: -7, padding:'5px'}} />
+                University HR
+            </NavbarBrand>
+
+            <SearchBox/>
+          
+            <NavbarToggler onClick={this.toggle}/>
+          
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
@@ -49,7 +55,7 @@ import logo from './logo.png';
                   <NavLink href="/Departments">Departments</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink href="Profile">Profile</NavLink>
+                  <NavLink href="/Profile">Profile</NavLink>
                 </NavItem>
                 
               </Nav>
--- client/src/UNVHR/Home.js
+++ client/src/UNVHR/Home.js
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import {SearchBox} from './SearchBox'
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 import './Home.css';
 
 
@@ -6,7 +10,21 @@ export class Home extends React.Component{
   render(){
     return (
         <div className="wrapper" >
-            <h1>This Is Home</h1>
+            <h1 className="welcomeText" >Welcome to HR System</h1>
+            <div className="findCard" >
+              <h2>Find an Employee</h2>
+              <Select className="droplistStyle"
+                 placeholder = "Choose a Department"
+              /> 
+              <Select className="droplistStyle"
+                 placeholder = "Choose Employee Name"
+              /> 
+
+              <button className="searchButton" >
+                Search
+              </button>
+
+            </div>
         </div>
              
     );
