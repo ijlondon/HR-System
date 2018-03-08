5e2560bc18150d7e879da811b4250820ba1ddbdf
ijlondon <ijl8216@rit.edu>
2018-03-08 17:37:43 -0500

    create jobTitleRepository

--- src/main/java/com/rit/group2/repositories/EmployeeRepository.java
+++ src/main/java/com/rit/group2/repositories/EmployeeRepository.java
@@ -7,9 +7,9 @@ import com.rit.group2.models.Address;
 import com.rit.group2.models.Employee;
 
 public class EmployeeRepository {
-	
+
 	private ArrayList<Employee> employees;
-	
+
 	private static EmployeeRepository employeeRepository;
 
 	public EmployeeRepository(){
@@ -18,15 +18,16 @@ public class EmployeeRepository {
 		employees.add(new Employee("Nathan", "Connor", new Address("300 Lomb Rd", "Rochester", "NY", 14580), "(585)156-5490", "nxc5929@rit.edu", 1));
 		employees.add(new Employee("Matt", "Anacelle", new Address("301 Lomb Rd", "Rochester", "NY", 14580), "(585)586-5975", "max5890@rit.edu", 1));
 		employees.add(new Employee("Kaylie", "Glynn", new Address("300 Lomb Rd", "Rochester", "NY", 14580), "(585)963-1005", "kcg2690@rit.edu", 1));
-		
+
 		// TODO: Remove for R2
 		int id = 0;
 		for (Employee employee : employees) {
 			employee.setId(id);
+			employee.setJobTitle("Education Teachers")
 			id++;
 		}
 	}
-	
+
 	public static EmployeeRepository getInstance(){
 		if(employeeRepository == null){
 			employeeRepository = new EmployeeRepository();
