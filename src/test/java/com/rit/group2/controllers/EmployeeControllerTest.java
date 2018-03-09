0bd8644d36c548d20be339a6a1c3f15141054929
Alex Michael Christodoulou Jr <amc8391@rit.edu>
2018-03-09 05:14:56 -0500

    Optimizing imports in tests

--- src/test/java/com/rit/group2/controllers/EmployeeControllerTest.java
+++ src/test/java/com/rit/group2/controllers/EmployeeControllerTest.java
@@ -1,29 +1,23 @@
-package com.test;
+package com.rit.group2.controllers;
 
-import java.util.ArrayList;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import com.rit.group2.HrSystemApplication;
+import com.rit.group2.responses.SuccessfulResponse;
+import com.rit.group2.services.EmployeeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import com.rit.group2.responses.SuccessfulResponse;
-
-import com.rit.group2.controllers.EmployeeController;
-import com.rit.group2.services.EmployeeService;
-import com.rit.group2.HrSystemApplication;
-import com.rit.group2.models.BasicEmployee;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ContextConfiguration(classes=HrSystemApplication.class)
 @RunWith(SpringRunner.class)
