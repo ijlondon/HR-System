d0d0e1ca366cf8249e5823331e58c6d9d5a81898
secretspy97 <nathan.connor@gosparx.org>
2018-04-29 19:14:38 -0400

    Added Tests back in

--- src/test/java/com/rit/group2/ApplicationTest.java
+++ src/test/java/com/rit/group2/ApplicationTest.java
@@ -1,11 +1,11 @@
 // package com.rit.group2;
-
+//
 // import static org.hamcrest.CoreMatchers.containsString;
 // import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 // import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 // import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 // import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+//
 // import org.junit.Test;
 // import org.junit.runner.RunWith;
 // import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +14,18 @@
 // import org.springframework.boot.test.context.SpringBootTest;
 // import org.springframework.test.context.junit4.SpringRunner;
 // import org.springframework.test.web.servlet.MockMvc;
-
+//
 // @RunWith(SpringRunner.class)
 // @SpringBootTest
 // @AutoConfigureMockMvc
 // public class ApplicationTest {
-
+//
 // 	@Autowired
 // 	private MockMvc mockMvc;
-   
-// //    @Test
-// //    public void shouldReturnDefaultMessage() throws Exception {
-// //        this.mockMvc.perform(get("/employee/0")).andDo(print()).andExpect(status().isOk())
-// //                .andExpect(content().string(containsString("Hello World")));
-// //    }
+//   
+//     @Test
+//     public void shouldReturnDefaultMessage() throws Exception {
+//         this.mockMvc.perform(get("/employee/0")).andDo(print()).andExpect(status().isOk())
+//                 .andExpect(content().string(containsString("Hello World")));
+//     }
 // }
--- src/test/java/com/rit/group2/DepartmentControllerTest.java
+++ src/test/java/com/rit/group2/DepartmentControllerTest.java
@@ -1,101 +1,97 @@
-//package com.rit.group2;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.mockito.Mockito.when;
-//
-//import java.util.ArrayList;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.rit.group2.controllers.DepartmentController;
-//import com.rit.group2.models.Address;
-//import com.rit.group2.models.Department;
-//import com.rit.group2.models.Employee;
-//import com.rit.group2.responses.Response;
-//import com.rit.group2.responses.SuccessfulResponse;
-//import com.rit.group2.services.DepartmentService;
-//
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(value = DepartmentController.class)
-//public class DepartmentControllerTest {
-//    
-//	@Autowired
-//	private MockMvc mvc;
-//	
-//	@Autowired
-//	private ObjectMapper objectMapper;
-//	
-//	@MockBean
-//	private DepartmentService departmentService;
-//	
-//	private Department department;
-//	private Employee head;
-//	private Employee worker;
-//	
-//	@Before
-//	public void setup(){
-//		worker = new Employee("Nathan", "Connor", new Address("300 Lomb Road", "Rochester", "NY", 14589), "(585)-589-5809", "nxc5929@rit.edu", 1, "tester");
-//		head = new Employee("Kaylie", "Glynn", new Address("300 Lomb Road", "Rochester", "NY", 14589), "(585)-589-3455", "kcg3450@rit.edu", 1, "tester");
-//		department = new Department("Testing");
-//	}
-//	
-//	@Test
-//	public void getAllDepartments() throws Exception{
-//		ArrayList<Department> departments = new ArrayList<>();
-//		departments.add(department);
-//		Response response = new SuccessfulResponse("Retrieved all Employees", departments);
-//		
-//		when(departmentService.getAll()).thenReturn(response);
-//		
-//		System.out.println("Testing /department...");
-//		mvc.perform(
-//				get("/department")
-//				.contentType(MediaType.APPLICATION_JSON)
-//				)
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("error", is(false)))
-//				.andExpect(jsonPath("data.[0].name", is(department.getName())));
-//	}
-//	
-//	@Test
-//	public void createDepartment() throws Exception{
-//		Response response = new SuccessfulResponse("Successfully created", department);
-//		
-//		when(departmentService.createDepartment(null)).thenReturn(response);
-//		System.out.println("Testing /department/create");
-//		mvc.perform(
-//				post("/department/create")
-//				.contentType(MediaType.APPLICATION_JSON)
-//				.content(objectMapper.writeValueAsString(department))
-//				)
-//				.andExpect(status().isOk());
-//	}
-//	
-//	@Test
-//	public void getDepartment() throws Exception{
-//		Response response = new SuccessfulResponse("Successfully created", department);
-//		
-//		when(departmentService.getDepartment(0)).thenReturn(response);
-//		System.out.println("Testing /department/0...");
-//		mvc.perform(
-//				get("/department/0")
-//				.contentType(MediaType.APPLICATION_JSON)
-//				)
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("data.name", is(department.getName())));
-//	}
-//	
-//}
+package com.rit.group2;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rit.group2.controllers.DepartmentController;
+import com.rit.group2.models.Address;
+import com.rit.group2.models.Department;
+import com.rit.group2.models.Employee;
+import com.rit.group2.responses.Response;
+import com.rit.group2.responses.SuccessfulResponse;
+import com.rit.group2.services.DepartmentService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = DepartmentController.class)
+public class DepartmentControllerTest {
+    
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@MockBean
+	private DepartmentService departmentService;
+	
+	private Department department;
+	
+	@Before
+	public void setup(){
+		department = new Department("Software Engineering");
+	}
+	
+	@Test
+	public void getAllDepartments() throws Exception{
+		ArrayList<Department> departments = new ArrayList<>();
+		departments.add(department);
+		Response response = new SuccessfulResponse("Retrieved all Departments", departments);
+		
+		when(departmentService.getAll()).thenReturn(response);
+		
+		System.out.println("Testing /department...");
+		mvc.perform(
+				get("/department")
+				.contentType(MediaType.APPLICATION_JSON)
+				)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("error", is(false)))
+				.andExpect(jsonPath("data.[0].name", is(department.getName())));
+	}
+	
+	@Test
+	public void createDepartment() throws Exception{
+		Response response = new SuccessfulResponse("Successfully created", department);
+		
+		when(departmentService.createDepartment(null)).thenReturn(response);
+		System.out.println("Testing /department/create");
+		mvc.perform(
+				post("/department/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(department))
+				)
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void getDepartment() throws Exception{
+		Response response = new SuccessfulResponse("Successfully created", department);
+		
+		when(departmentService.getDepartment(0)).thenReturn(response);
+		System.out.println("Testing /department/0...");
+		mvc.perform(
+				get("/department/0")
+				.contentType(MediaType.APPLICATION_JSON)
+				)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("data.name", is(department.getName())));
+	}
+	
+}
