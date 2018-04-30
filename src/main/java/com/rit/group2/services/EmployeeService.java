36f10be17e883c4dfe9e01224ff9e06d36ab85f4
secretspy97 <nathan.connor@gosparx.org>
2018-04-30 07:16:47 -0400

    Added init to alwasy recreate database

--- src/main/java/com/rit/group2/services/EmployeeService.java
+++ src/main/java/com/rit/group2/services/EmployeeService.java
@@ -33,9 +33,8 @@ public class EmployeeService {
 
 
 	public Response init(){
-		if(employeeRepository.findByFirstAndLastName("Nathan", "Connor") != null){
-			return new ErrorResponse("Already Init");
-		}
+		employeeRepository.deleteAll();
+		departmentRepository.deleteAll();
 
 		Department department0 = new Department("Administration");		
 		Department department1 = new Department("Software Engineering");
